37fda71f3dfc30cba4a9bff2564916965a994c61
sepehra90 <sepehra90@gmail.com>
2024-03-14 18:55:40 +0330

    v 0.0.0

---
+++ main.py
@@ -0,0 +1,9 @@
+import customtkinter as ctk
+
+
+if __name__ == "__main__":
+    root = ctk.CTk()
+    root.title("Neo DNS Changer With Python by Red Mage")
+    root.geometry("450x540")
+
+    root.mainloop()
