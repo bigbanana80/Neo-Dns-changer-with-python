f632f1cd7e19bd805db348cc612e98d1b041d6b7
sepehra90 <sepehra90@gmail.com>
2024-03-15 18:07:40 +0330

    v 0.0.4 "added more ui elements"

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import customtkinter as ctk
+import subprocess
 
 
 class main_app:
@@ -7,31 +8,42 @@ class main_app:
         self.root.title("Neo DNS Changer With Python by Red Mage")
         # ? section 1 which is general buttons like activate or delete the selected dns or cmd commands
         self.section_1_frame = ctk.CTkFrame(master=self.root)
-        self.section_1_frame.grid(row=0, column=0, columnspan=1, rowspan=1)
+        self.section_1_frame.grid(row=0, column=0, columnspan=1, rowspan=1, sticky="nw")
+
         self.btn_activate = ctk.CTkButton(
             master=self.section_1_frame, text="Activate"
-        ).pack(padx=10, pady=10)
+        ).grid(row=0, column=0, columnspan=1, rowspan=1, padx=10, pady=10)
         self.btn_delete = ctk.CTkButton(
             master=self.section_1_frame, text="Delete"
-        ).pack(padx=10, pady=10)
+        ).grid(row=0, column=1, columnspan=1, rowspan=1, padx=10, pady=10)
         self.btn_renew = ctk.CTkButton(
-            master=self.section_1_frame, text="Renew IP"
-        ).pack(padx=10, pady=10)
+            master=self.section_1_frame, text="Renew IP(auto release)"
+        ).grid(row=1, column=0, columnspan=1, rowspan=1, padx=10, pady=10)
         self.btn_flush = ctk.CTkButton(
             master=self.section_1_frame, text="Flush Dns"
-        ).pack(padx=10, pady=10)
+        ).grid(row=1, column=1, columnspan=1, rowspan=1, padx=10, pady=10)
+        self.auto_renew_checkbox = ctk.CTkCheckBox(
+            master=self.section_1_frame, text="Auto Renew ip"
+        ).grid(row=2, column=0, columnspan=1, rowspan=1, padx=10, pady=10)
+        self.auto_flush_checkbox = ctk.CTkCheckBox(
+            master=self.section_1_frame, text="Auto flush"
+        ).grid(row=2, column=1, columnspan=1, rowspan=1, padx=10, pady=10)
+        self.btn_dhcp_dns = ctk.CTkButton(
+            master=self.section_1_frame, text="DHCP Dns"
+        ).grid(row=3, column=0, columnspan=1, rowspan=1, padx=10, pady=10)
+
         # ? section 2 which is a list of dns profiles which we can use, manipulate and add new ones at will
         self.section_2_frame = ctk.CTkFrame(master=self.root)
-        self.section_2_frame.grid(row=0, column=1, columnspan=1, rowspan=1)
-        self.lb_list_info = ctk.CTkLabel(
+        self.section_2_frame.grid(row=0, column=1, columnspan=1, rowspan=1, sticky="sw")
+        self.lb_s2_list_info = ctk.CTkLabel(
             master=self.section_2_frame, text="Name \t DNS1 \t DNS2"
         ).pack(padx=10, pady=10)
-        self.ls_dns_frame = ctk.CTkScrollableFrame(
+        self.ls_s2_dns_frame = ctk.CTkScrollableFrame(
             master=self.section_2_frame, height=300
         ).pack(padx=10, pady=10)
         # ? section 3  which is a for adding new dns profiles and editing existing ones
         self.section_3_frame = ctk.CTkFrame(master=self.root)
-        self.section_3_frame.grid(row=1, column=0, columnspan=1, rowspan=1)
+        self.section_3_frame.grid(row=1, column=0, columnspan=1, rowspan=1, sticky="sw")
         self.lb_s3_name = ctk.CTkLabel(master=self.section_3_frame, text="Name:").grid(
             row=0, column=0, padx=10, pady=10
         )
@@ -51,15 +63,19 @@ class main_app:
             master=self.section_3_frame, placeholder_text="Valid DNS like 1.0.0.1"
         ).grid(row=2, column=1, padx=10, pady=10)
         self.btn_add = ctk.CTkButton(master=self.section_3_frame, text="ADD").grid(
-            row=3, column=0
+            row=3, column=0, padx=10, pady=10
         )
         self.btn_edit = ctk.CTkButton(master=self.section_3_frame, text="EDIT").grid(
-            row=3, column=1
+            row=3, column=1, padx=10, pady=10
         )
         # ? section 4 which just shows current DNS config that is active by the host pc and
         # ? also the section where you can choose the Network adapter which by defualt is Wi-fi
         self.section_4_frame = ctk.CTkFrame(master=self.root)
-        self.section_4_frame.grid(row=1, column=1, columnspan=1, rowspan=1)
+        self.section_4_frame.grid(row=1, column=1, columnspan=1, rowspan=1, sticky="nw")
+
+        self.adapter_selector = ctk.CTkOptionMenu(master=self.section_4_frame).grid(
+            row=0, column=0, columnspan=1, rowspan=1
+        )
 
 
 if __name__ == "__main__":
